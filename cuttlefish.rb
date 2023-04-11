d0ed77f9a918589aee334d7b5a2eaa5edfa83b9a
Kuba Martin <jakub.wit.martin@gmail.com>
2023-04-12 01:49:19 +0200

    Update cuttlefish.rb

--- cuttlefish.rb
+++ cuttlefish.rb
@@ -6,16 +6,16 @@ class Cuttlefish < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/cube2222/cuttlefish/releases/download/v0.1.0/cuttlefish_darwin_amd64.zip"
-      sha256 "bae68c1ed9f2664f4155496068e40bbe2ea1ca91658be542249865d6b29c6123"
+      url "https://github.com/cube2222/cuttlefish/releases/download/v0.1.1/cuttlefish_darwin_amd64.zip"
+      sha256 "9640a0f7ffe7046571d9cfac2529d02f560d254a35c492aedbb56990795a3004"
 
       def install
         bin.install "cuttlefish"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/cube2222/cuttlefish/releases/download/v0.1.0/cuttlefish_darwin_arm64.zip"
-      sha256 "aabeca44be6ba533ec8abd7b6e4721f529efaa6aeb1ac537d2c81d25185c84e3"
+      url "https://github.com/cube2222/cuttlefish/releases/download/v0.1.1/cuttlefish_darwin_arm64.zip"
+      sha256 "b8fae160be7f7b6e2a67eb0caa4ce6e56e47f3fbcc6b863034726b4623dcae13"
 
       def install
         bin.install "cuttlefish"
